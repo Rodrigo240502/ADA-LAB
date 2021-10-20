cd5fce41c9c48bf109073d3f8bf9fd73c0102dc0
Rodrigo Guerra <72612781+Rodrigo240502@users.noreply.github.com>
2021-10-20 11:59:24 -0500

    Create Intersect.java

---
+++ Intersect.java
@@ -0,0 +1,26 @@
+public class Interseccion {
+
+    public static void main(String[] args) {
+
+        int[] a = { 2, 4, 5, 1, 8, 9 };
+        int[] b = { 1, 2, 4, 6, 7 };
+        System.out.println(intersect(a,b));
+    }
+    public static ArrayList<Integer> intersect(int[]a, int[]b){
+        int iterar = a.length*b.length;
+        int j=0;
+        int k=0;
+        ArrayList<Integer> repetidos = new ArrayList<Integer>();
+        for(int i=0;i<iterar;i++){
+            if(j<a.length && k<b.length){
+                if(a[j]==b[k]){
+                    repetidos.add(a[j]);
+                    j++;
+                }
+                k++;
+            }
+        }
+        return repetidos;
+    }
+
+}
