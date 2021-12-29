0e0a6ccc7739ee6e7d048525ba4abd3b0d54e6ca
RodrigoGuerra <rodrigotyg@gmail.com>
2021-12-29 15:11:59 -0500

    SpellingBee y GruopAnagrasm documentado

--- Lab11/GroupAnagrams.py
+++ Lab11/GroupAnagrams.py
@@ -1,10 +1,31 @@
+
+# Logica de la solucion: se nos daran varias String dentro de una lista
+# y nos pide separarlos en grupos que sean anagramas, mi idea fue ordenar 
+# esa palabra ya que al ordenarlas todas son iguales y ya que en python no
+# hay como tal un hashMap, usare un diccionario de clave valor para guardar
+# las palabras que ordenadas sean iguales en la clave que seria la palabra
+# ordenada.
+
+# Recibimos un arreglo con las palabras
 def groupAnagrams(strs):
-    
+    # Creamos un diccionario(igual a un hashMap en Java) donde 
+    # se guarda clave y valor. 
     anagrams = {}
+
+    # Iteramos dentro del arreglo dado de palabras
     for word in strs:
-        sortedWord = "".join(sorted(word))
+        # Ordenamos la palabra
+        sortedWord = sorted(word)
+
+        # Si la palabra ordenada ya esta como clave en el diccionario 
+        # agregaremos su valor que seria la palabra no ordenada en la 
+        # clave que seria la palabra ordenada.
         if sortedWord in anagrams:
             anagrams[sortedWord].append(word)
+        
+        # De lo contrario crearemos un array con la palabra normal
         else:
             anagrams[sortedWord] = [word]
+
+    # Finalmente retornamos el diccionario como una lista
     return list(anagrams.values())
--- Lab11/SpellingBee.java
+++ Lab11/SpellingBee.java
@@ -1,50 +1,49 @@
 package Lab11;
 import java.util.Scanner;
-import java.lang.String;
 
 public class SpellingBee {
 
-    public static Boolean isValid(String word, String givenLetters) {
+    public static Boolean isValid(String word, String charsDados) {
         // Inicializamos la variable para saber si contiene un char especifico
-        boolean containsSpecifcCharacter = false;
+        boolean contieneChar = false;
 
-		// Guardamos el char dado como param
-		char startingLetter = givenLetters.charAt(0);
+		// Guardamos el primer char dado en el parametro charsDados
+		char primerChar = charsDados.charAt(0);
 
 		// Verificamos si la palabra tiene el largo minimo
 		if(word.length() < 4)
 			return false;
 
 		// Convertimos la palabra a un array de Chars
-		char[] wd = word.toCharArray();
+		char[] wordChars = word.toCharArray();
 
 		// Iteramos los char del array 
-		for(char c : wd) {
+		for(char c : wordChars) {
 			// Revisamos si esta dentro de los 7 char dados
-			if(givenLetters.indexOf(c) == -1)
+			if(charsDados.indexOf(c) == -1)
 				return false;
 
 			//Si conntiene un char cambiamos la bandera a true
-			if(!containsSpecifcCharacter && c == startingLetter)
-				containsSpecifcCharacter = true;
+			if(!contieneChar && c == primerChar)
+				contieneChar = true;
 		}
 
-		return containsSpecifcCharacter;
+		return contieneChar;
 	}
 
 	public static void main(String args[]) {
 
 		Scanner sc = new Scanner(System.in);
 
-		String givenLetters = sc.nextLine(), word;
+		String charsDados = sc.nextLine(), word;
 		int inputSize = Integer.valueOf(sc.nextLine());
 
 		// Ingreso de palabras
 		for(int i = 0; i < inputSize; i++) {
 			word = sc.nextLine();
 
-			// Verificamos si es valida
-			if(isValid(word, givenLetters))
+			// Verificamos si es valida en los chars dados
+			if(isValid(word, charsDados))
 				System.out.println(word);
 		}
 
